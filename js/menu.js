5b6d9f73e3646529c7b3474ac2a6d71d621f41c9
Andre Brito <atb.tavares@gmail.com>
2017-06-07 23:11:04 -0400

    correcao menu inicial

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,22 @@
 "use strict"; var MenuState = function(game) {}; 
+
 MenuState.prototype.preload = function() 
 {
-   game.load.image('bg', 'assets/inicio.jpg');
-   game.load.spritesheet('btn', 'assets/btn_inicio.png',283,118);
+   game.load.image('bg', 'assets/tela_inicial_2.jpg');
+   game.load.spritesheet('btn', 'assets/btn_inicio.png',200,35);
    game.load.image('creditos', 'assets/creditos.jpg');
 }
 
 MenuState.prototype.create = function() 
 {
    game.add.tileSprite(0, 0, 800, 600, 'bg');
+   this.sprite_creditos =  game.add.sprite(0, 0, 'creditos');
+   this.sprite_creditos.visible = false;
    this.cursors = game.input.keyboard.createCursorKeys();
    this.enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
    this.escKey = game.input.keyboard.addKey(Phaser.Keyboard.ESCAPE);
-   this.sprite_creditos = game.add.sprite(0,0,'creditos');
-   this.sprite_creditos.visible = false;
-   this.btn_jogar= game.add.button(200, 210, 'btn',this.startGame, this, 2, 1, 0);
-   this.btn_creditos= game.add.button(200, 270, 'btn',this.showTeam, this, 2, 1, 0);
+   this.btn_jogar= game.add.button(205, 242, 'btn',this.startGame, this, 2, 1, 0);
+   this.btn_creditos= game.add.button(205, 302, 'btn',this.actionOnClick, this, 2, 1, 0);
 }
 
 
@@ -25,10 +26,12 @@ MenuState.prototype.update = function()
    {
    	this.game.state.start('game');
    }
+   /*
    if (this.escKey.isDown);
    {
    	this.sprite_creditos.visible = false;
    }
+   */
 }
 
 MenuState.prototype.startGame= function()
@@ -36,13 +39,15 @@ MenuState.prototype.startGame= function()
    this.game.state.start('game');
 }
 
-MenuState.prototype.showTeam= function()
+MenuState.prototype.actionOnClick= function()
 {
-   this.sprite_creditos.visible = true;
+   this.sprite_creditos.visible =! this.sprite_creditos.visible;
+   if (this.sprite_creditos.visible){
+   	btn_creditos.reset(205,302);
+   } else { 
+   	btn_creditos.reset(10,10);
+   }
 }
 
-MenuState.prototype.hideTeam = function()
-{
-   this.sprite_creditos.visible = false;
-}
-
+/*
+*/
